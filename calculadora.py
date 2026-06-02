4528b2f3950542cbc8b94f8b3440965fa8a0ffe9
Ariel Dias <arielsdias@gmail.com>
2026-06-02 14:54:06 -0300

    teste456

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,9 @@
-def somar(a, b):
-    return a + b
+from calculadora import somar, multiplicar
+from config import FEATURE_MULTIPLICACAO
+
+print("Soma:", somar(2, 3))
+
+if FEATURE_MULTIPLICACAO:
+    print("Multiplicação:", multiplicar(2, 3))
+else:
+    print("Multiplicação indisponível")
